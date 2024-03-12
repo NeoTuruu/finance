50f5e4908c0ccfa107b85cf0af5ab0836ddfaf44
NeoTuruu <neo.orgil@gmail.com>
2024-03-13 00:10:33 +0800

    tusviig delgetsend update hiine

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var uiController = (function () {
     addBtn: ".add__btn",
     incomeList: ".income__list",
     expenseList: ".expenses__list",
+    tusuvLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -36,7 +40,20 @@ var uiController = (function () {
       //   fieldsArr[i].value = "";
       // }
     },
-
+    tusviigUzuuleh: function (tusuv) {
+      document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+      document.querySelector(DOMstrings.incomeLabel).textContent =
+        tusuv.totalInc;
+      document.querySelector(DOMstrings.expenseLabel).textContent =
+        tusuv.totalExp;
+      if (tusuv.huvi !== 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi + "%";
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent =
+          tusuv.huvi;
+      }
+    },
     addListItem: function (item, type) {
       // orlogo zarlagiin elementiig aguulsan html-g beltgene
       var html, list;
@@ -167,7 +184,7 @@ var appController = (function (uiController, financeController) {
       // 5. Etsiin uldegdel
       var tusuv = financeController.tusviigAvah();
       // 6.Tusuv tootsoog delgetsend gargana
-      console.log(tusuv);
+      uiController.tusviigUzuuleh(tusuv);
     }
   };
 
@@ -188,6 +205,12 @@ var appController = (function (uiController, financeController) {
   return {
     init: function () {
       console.log("Application started...");
+      uiController.tusviigUzuuleh({
+        tusuv: 0,
+        huvi: 0,
+        totalInc: 0,
+        totalExp: 0,
+      });
       setupEventListeners();
     },
   };
