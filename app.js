e68dbf678c4e0a055080744a4e037fea60b2e14d
NeoTuruu <neo.orgil@gmail.com>
2024-03-12 13:51:00 +0800

    Orlogo zarlagiig delgetsen deer gargana

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var uiController = (function () {
     inputValue: ".add__value",
     addBtn: ".add__btn",
   };
+
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
@@ -18,69 +19,108 @@ var uiController = (function () {
     getDOMstrings: function () {
       return DOMstrings;
     },
+
+    addListItem: function (item, type) {
+      // orlogo zarlagiin elementiig aguulsan html-g beltgene
+      var html, list;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete">            <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div>        </div></div>';
+      } else {
+        list = ".expenses__list";
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div>          <div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn">                <i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // Ter HTML dotroo orlogo zarlagiin utguudiig REPLACE ashiglaj uurchilj ugnu
+      html = html.replace("%id%", item.id);
+      html = html.replace("$$DESCRIPTION$$", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      // Beltgesen HTML-ee DOM-ruu hiij ugnu.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
   };
 })();
 
 // Sanhuutei ajillah controller
 var financeController = (function () {
-  // private baiguulagch function
+  // private data
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
-  // private baiguulagch function
+
+  // private data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+
   // private data
   var data = {
     items: {
       inc: [],
       exp: [],
     },
+
     totals: {
       inc: 0,
       exp: 0,
     },
   };
+
   return {
     addItem: function (type, desc, val) {
       var item, id;
+
       if (data.items[type].length === 0) id = 1;
       else {
         id = data.items[type][data.items[type].length - 1].id + 1;
       }
+
       if (type === "inc") {
         item = new Income(id, desc, val);
       } else {
-        //type===exp
         item = new Expense(id, desc, val);
       }
-      data.items[type].push();
+
+      data.items[type].push(item);
+
+      return item;
     },
+
     seeData: function () {
       return data;
     },
   };
 })();
 
-// Programiin holbogch controller
+// // Programiin holbogch controller
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1. Oruulah ugugdliig delgetsees orlj avna
     var input = uiController.getInput();
 
     // 2. Olj avsan ugugdluudee sanhuugiin controllerluu damjuulj tendee hadgalna
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
     // 3. Olj avsan ugugdluudee web deeree tohiroh hesegt ni gargana.
+    uiController.addListItem(item, input.type);
+
     // 4. Tusviig tootsoolno
     // 5. Etsiin uldegdel tootsoog delgetsen gargana
   };
-  var setupEventListener = function () {
+
+  var setupEventListeners = function () {
     var DOM = uiController.getDOMstrings();
+
     document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
     });
@@ -95,7 +135,7 @@ var appController = (function (uiController, financeController) {
   return {
     init: function () {
       console.log("Application started...");
-      setupEventListener();
+      setupEventListeners();
     },
   };
 })(uiController, financeController);
